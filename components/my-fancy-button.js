6ce7ac341362f1a7b3675c6d5cc9f32018372245
Ameer Jhan <ameerjhanprof@gmail.com>
2022-10-19 11:01:25 +0530

    initial commit

---
+++ components/my-fancy-button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'ui-kit/hrds/button';
+
+export default function MyFancyButton() {
+  return (
+    <Button
+      variant="primary"
+    >
+      Don't click me!
+    </Button>
+  )
+}
---
+++ components/my-fancy-input.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Input } from 'ui-kit/hrds/input';
+
+export default function MyFancyButton() {
+  return (
+    <Input />
+  )
+}
